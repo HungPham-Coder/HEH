c95d92148c376c2d340f90ebfe20b6b0a6228523
kenly <hungppmse140153@fpt.edu.vn>
2023-02-20 20:08:10 +0700

    update

---
+++ heh_application/lib/login.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:heh_application/forgotPass.dart';
+import 'package:heh_application/signup.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 25,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Column(
+                  children: const <Widget>[
+                    Text(
+                      "LOGIN",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputPhone(label: ""),
+                      inputPassword(label: "", obscureText: true)
+                    ],
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return const ForgotPassword();
+                              }),
+                            );
+                          },
+                          child: const Text(
+                            "Forgot Password?",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 46, 161, 226),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: const Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                        )),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {},
+                      color: const Color.fromARGB(255, 46, 161, 226),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: const <Widget>[
+                //     Text("Don't have an account yet?"),
+                //     Text("Sign up",
+                //         style: TextStyle(
+                //           color: Color.fromARGB(255, 46, 161, 226),
+                //           fontWeight: FontWeight.bold,
+                //         )),
+                //   ],
+                // ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text("Doesn't have account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return const SignUpPage();
+                              }),
+                            );
+                          },
+                          child: const Text(
+                            "Sign up ",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 46, 161, 226),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const Text("now!")
+                      ],
+                    )),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//create text field
+Widget inputPhone({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Phone number',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget inputPassword({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Password',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
---
+++ heh_application/lib/signup.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            size: 25,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          height: MediaQuery.of(context).size.height - 50,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  const Text(
+                    "SIGN UP",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Create an account. It's Free!",
+                    style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  fullname(label: ""),
+                  surName(label: ""),
+                  email(label: ""),
+                  phone(label: ""),
+                  dob(label: ""),
+                  password(label: "", obscureText: true),
+                  confirmPassword(label: "", obscureText: true),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget fullname({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'First name',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget surName({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Sur number',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget email({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Email',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget phone({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Phone number',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget dob({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'DD/MM/YYYY',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget password({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Password',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
+
+Widget confirmPassword({label, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+      ),
+      const SizedBox(height: 5),
+      TextField(
+        obscureText: obscureText,
+        decoration: const InputDecoration(
+            hintText: 'Confirm Password',
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+      const SizedBox(height: 10)
+    ],
+  );
+}
