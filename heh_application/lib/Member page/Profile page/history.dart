85cb287b25aeb507853b6ddef84c7cfb40a3136a
kenly <hungppmse140153@fpt.edu.vn>
2023-04-12 22:43:09 +0700

    update

--- heh_application/lib/Member page/Profile page/history.dart
+++ heh_application/lib/Member page/Profile page/history.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -14,14 +13,92 @@ class _HistoryPageState extends State<HistoryPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: const Text(
           "Lịch sử giao dịch",
           style: TextStyle(fontSize: 23),
         ),
-        centerTitle: true,
         elevation: 10,
         backgroundColor: const Color.fromARGB(255, 46, 161, 226),
+      ),
+    );
+  }
+}
+
+class PhysioChooseMenu extends StatelessWidget {
+  const PhysioChooseMenu({
+    Key? key,
+    required this.time,
+    required this.name,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  final String icon, name, time;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: duplicate_ignore
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 15)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: const BorderSide(
+                              color: Color.fromARGB(255, 46, 161, 226),
+                              width: 2)),
+                    )),
+                onPressed: press,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.network(
+                      icon,
+                      width: 40,
+                      height: 50,
+                    ),
+                    const SizedBox(width: 8),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width / 1.6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              name,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              time,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ],
+                        )),
+                    Column(
+                      children: [
+                        Icon(Icons.arrow_forward_ios_rounded),
+                        SizedBox(height: 5),
+                        Icon(Icons.arrow_forward_ios_rounded),
+                      ],
+                    ),
+                  ],
+                )),
+          ),
+        ],
       ),
     );
   }
