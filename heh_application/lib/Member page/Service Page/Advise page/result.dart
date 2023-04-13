98567db2115f89a7b931bf34557edb8b82cec1d9
kenly <hungppmse140153@fpt.edu.vn>
2023-04-13 13:29:14 +0700

    update

--- heh_application/lib/Member page/Service Page/Advise page/result.dart
+++ heh_application/lib/Member page/Service Page/Advise page/result.dart
@@ -21,34 +21,28 @@ class _TimeResultPageState extends State<TimeResultPage> {
           elevation: 10,
           backgroundColor: const Color.fromARGB(255, 46, 161, 226),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.circular(10)),
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              const SizedBox(height: 10),
-              const Center(
-                child: Text("Danh sách chuyên viên",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                    )),
-              ),
+              const SizedBox(height: 20),
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: PhysioChooseMenu(
-                  icon:
-                      "https://firebasestorage.googleapis.com/v0/b/healthcaresystem-98b8d.appspot.com/o/icon%2Fphy.png?alt=media&token=bac867bc-190c-4523-83ba-86fccc649622",
-                  name: "Phạm Phú Minh Hưng",
-                  time: "Khung giờ: ",
-                  press: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => BillTimePage()));
-                  },
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    CurrentTime(),
+                    PhysioChooseMenu(
+                      icon:
+                          "https://firebasestorage.googleapis.com/v0/b/healthcaresystem-98b8d.appspot.com/o/icon%2Fphy.png?alt=media&token=bac867bc-190c-4523-83ba-86fccc649622",
+                      name: "Phạm Phú Minh Hưng",
+                      time: "Khung giờ: 6:00 - 7:00",
+                      press: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const BillTimePage()));
+                      },
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -72,63 +66,87 @@ class PhysioChooseMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: duplicate_ignore
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: TextButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 15)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: const BorderSide(
+                            color: Color.fromARGB(255, 46, 161, 226),
+                            width: 2)),
+                  )),
+              onPressed: press,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.network(
+                    icon,
+                    width: 40,
+                    height: 50,
+                  ),
+                  const SizedBox(width: 5),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.6,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            name,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            time,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ],
+                      )),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Icon(Icons.arrow_forward_ios_rounded),
+                  ),
+                ],
+              )),
+        ),
+      ],
+    );
+  }
+}
+
+class CurrentTime extends StatelessWidget {
+  CurrentTime({Key? key}) : super(key: key);
+
+  final TextEditingController _date = TextEditingController();
+
+  String getCurrentDate() {
+    var date = DateTime.now().toString();
+
+    var dateParse = DateTime.parse(date);
+
+    var formattedDate =
+        "Ngày ${dateParse.day} Tháng ${dateParse.month} Năm ${dateParse.year}";
+    return formattedDate.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: TextButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 15)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: const BorderSide(
-                              color: Color.fromARGB(255, 46, 161, 226),
-                              width: 2)),
-                    )),
-                onPressed: press,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      icon,
-                      width: 40,
-                      height: 50,
-                    ),
-                    const SizedBox(width: 5),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width / 1.6,
-                        height: 50,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              name,
-                              style: Theme.of(context).textTheme.bodyText1,
-                            ),
-                            const SizedBox(height: 10),
-                            Text(
-                              time,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ],
-                        )),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Icon(Icons.arrow_forward_ios_rounded),
-                    ),
-                  ],
-                )),
-          ),
+        children: <Widget>[
+          Text(getCurrentDate(), style: const TextStyle(fontSize: 20))
         ],
       ),
     );
